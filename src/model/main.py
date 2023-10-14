fd0169f22cc45f52f3d0f07e794a648bb2152233
Florent-COMPAGNONI <florent.compagnoni+dev@gmail.com>
2023-10-14 18:06:51 +0200

    stemming & tests report on RandomForest

--- src/model/main.py
+++ src/model/main.py
@@ -1,127 +1,12 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.pipeline import Pipeline
-from sklearn.base import BaseEstimator, TransformerMixin
 
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.tokenize import sent_tokenize
-from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer
-
-from bs4 import BeautifulSoup
-from unidecode import unidecode
-import re
-import string
-import numpy as np
-import contractions
+from features.nltk_preprocessing import NltkTextPreprocessor
 
 def make_model():
     return Pipeline([
-        ("nltk_preprocessor", NltkTextPreprocessor() )
+        ("nltk_preprocessor", NltkTextPreprocessor() ),
         ("count_vectorizer", CountVectorizer()),
         ("random_forest", RandomForestClassifier()),
     ])
-
-class NltkTextPreprocessor(TransformerMixin, BaseEstimator):
-  def __init__(self):
-    pass
-
-  def fit(self, X):
-    return self
-
-  def transform(self, X):
-    txt_preproc = NltkPreprocessingSteps(X.copy())
-    processed_text = \
-            txt_preproc \
-            .replace_diacritics()\
-            .expand_contractions()\
-            .remove_numbers()\
-            .remove_punctuations_except_periods()\
-            .remove_double_spaces()\
-            .remove_all_punctuations()\
-            .remove_stopwords()\
-            .get_processed_text()
-
-    return processed_text
-
-def download_if_non_existent(res_path, res_name):
-  try:
-    nltk.data.find(res_path)
-  except LookupError:
-    print(f'resource {res_path} not found. Downloading now...')
-    nltk.download(res_name)
-
-class NltkPreprocessingSteps:
-  def __init__(self, X):
-    self.X = X
-    download_if_non_existent('corpora/stopwords', 'stopwords')
-    download_if_non_existent('tokenizers/punkt', 'punkt')
-    download_if_non_existent('taggers/averaged_perceptron_tagger',
-                             'averaged_perceptron_tagger')
-    download_if_non_existent('corpora/wordnet', 'wordnet')
-    download_if_non_existent('corpora/omw-1.4', 'omw-1.4')
-
-    self.sw_nltk = stopwords.words('french')
-    new_stopwords = ['<*>']
-    self.sw_nltk.extend(new_stopwords)
-    #self.sw_nltk.remove('not')
-
-    # '!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~' 32 punctuations in python
-    # we dont want to replace . first time around
-    self.remove_punctuations = string.punctuation.replace('.','')
-
-  def remove_html_tags(self):
-    self.X = self.X.apply(
-            lambda x: BeautifulSoup(x, 'html.parser').get_text())
-    return self
-
-  def replace_diacritics(self):
-    self.X = self.X.apply(
-            lambda x: unidecode(x, errors="preserve"))
-    return self
-
-  def to_lower(self):
-    self.X = np.apply_along_axis(lambda x: x.lower(), self.X)
-    return self
-
-  def expand_contractions(self):
-    self.X = self.X.apply(
-            lambda x: " ".join([contractions.fix(expanded_word) 
-                        for expanded_word in x.split()]))
-    return self
-
-  def remove_numbers(self):
-    self.X = self.X.apply(lambda x: re.sub(r'\d+', '', x))
-    return self
-
-  def replace_dots_with_spaces(self):
-    self.X = self.X.apply(lambda x: re.sub("[.]", " ", x))
-    return self
-
-  def remove_punctuations_except_periods(self):
-    self.X = self.X.apply(
-                 lambda x: re.sub('[%s]' %
-                  re.escape(self.remove_punctuations), '' , x))
-    return self
-
-  def remove_all_punctuations(self):
-    self.X = self.X.apply(lambda x: re.sub('[%s]' %
-                          re.escape(string.punctuation), '' , x))
-    return self
-
-  def remove_double_spaces(self):
-    self.X = self.X.apply(lambda x: re.sub(' +', ' ', x))
-    return self
-
-  def remove_stopwords(self):
-    # remove stop words from token list in each column
-    self.X = self.X.apply(
-            lambda x: " ".join([ word for word in x.split() 
-                     if word not in self.sw_nltk]) )
-    return self
-
-  def get_processed_text(self):
-    return self.X
-
